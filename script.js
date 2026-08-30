d3f1bc61395b15fce23fa751d6f78570a303694b
morganhill2323-jpg <morganhill2323@gmail.com>
2026-08-30 07:46:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let score = 0;
 let timeLeft = 30;
-
+setInterval(function() {
+    timeLeft = timeLeft - 1;
+    document.getElementById("timer").textContent = timeLeft;
+}, 1000);
 function addPoint() {
     score = score + 1;
     document.getElementById("score").textContent = score;
