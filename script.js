057deea3bc22c5d0ed61a756a71bf4ffd7632bd8
morganhill2323-jpg <morganhill2323@gmail.com>
2026-08-30 13:18:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let score = 0;
 let timeLeft = 30;
-setInterval(function() {
+let gameOver = false;
+
+function addPoint() {
+    if (gameOver === false) {
+        score = score + 1;
+        document.getElementById("score").textContent = score;
+    }
+}
+
+let timer = setInterval(function() {
     if (timeLeft > 0) {
         timeLeft = timeLeft - 1;
         document.getElementById("timer").textContent = timeLeft;
     }
+
+    if (timeLeft === 0 && gameOver === false) {
+        gameOver = true;
+        clearInterval(timer);
+        alert("Game Over! Your score was " + score);
+    }
 }, 1000);
-function addPoint() {
-    score = score + 1;
-    document.getElementById("score").textContent = score;
-}
