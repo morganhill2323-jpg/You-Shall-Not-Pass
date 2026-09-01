9b5d237b016f55424b494a7ae9b013ec070e8f89
morganhill2323-jpg <morganhill2323@gmail.com>
2026-09-01 15:12:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,237 @@
-let score = 0;
-let timeLeft = 30;
-let gameOver = false;
+<html lang="en"><head><title>Play Slope Game Online !</title><meta charset="utf-8"><meta name="robots" content="noindex, nofollow, noodp, noydir"><meta name="viewport" content="width=device-width, maximum-scale=1.0, initial-scale=1.0, user-scalable=no, minimal-ui"><meta http-equiv="X-UA-Compatible" content="requiresActiveX=true,IE=Edge,chrome=1"><meta http-equiv="Content-Language" content="en-US"><style>* {
+ margin: 0;
+ padding: 0;
+ box-sizing: border-box;
+ font-family: "Helvetica Neue", "Calibri Light", Roboto, sans-serif;
+ }
 
-function addPoint() {
-    if (gameOver === false) {
-        score = score + 1;
-        document.getElementById("score").textContent = score;
-    }
-}
+ #missing-flash {
+ display: none;
+ text-align: center
+ }
 
-let timer = setInterval(function() {
-    if (timeLeft > 0) {
-        timeLeft = timeLeft - 1;
-        document.getElementById("timer").textContent = timeLeft;
-    }
+ .fl-wrap {
+ margin: 0 auto;
+ background-color: #FFF;
+ padding: 20px;
+ position: absolute;
+ height: 100%;
+ width: 100%;
+ z-index: 9999;
+ }
 
-    if (timeLeft === 0 && gameOver === false) {
-        gameOver = true;
-        clearInterval(timer);
+ .fl-content {
+ color: #fff
+ }
 
-        document.getElementById("final-score").textContent = score;
-        document.getElementById("game-over").style.display = "block";
-    }
-}, 1000);
-function restartGame() {
-    score = 0;
-    timeLeft = 30;
-    gameOver = false;
+ .fl-game {
+ display: flex;
+ height: 95%;
+ justify-content: center;
+ align-items: center;
+ }
 
-    document.getElementById("score").textContent = score;
-    document.getElementById("timer").textContent = timeLeft;
-    document.getElementById("game-over").style.display = "none";
+ .fl-game a {
+ position: absolute;
+ z-index: 9999;
+ text-decoration: none
+ }
 
-    timer = setInterval(function() {
-        if (timeLeft > 0) {
-            timeLeft = timeLeft - 1;
-            document.getElementById("timer").textContent = timeLeft;
-        }
+ .fl-game span {
+ color: #FFF;
+ background-color: #3281ff;
+ padding: 10px 15px;
+ border-radius: 5px;
+ cursor: pointer;
+ font-weight: bold;
+ text-transform: uppercase;
+ }
 
-        if (timeLeft === 0 && gameOver === false) {
-            gameOver = true;
-            clearInterval(timer);
+ .fl-game span:hover {
+ background-color: #009cff
+ }
 
-            document.getElementById("final-score").textContent = score;
-            document.getElementById("game-over").style.display = "block";
-        }
-    }, 1000);
-}
+ .missing-flash-bg {
+ position: absolute;
+ top: 0;
+ left: 0;
+ width: 100%;
+ height: 100%;
+ -webkit-filter: blur(0.8em);
+ filter: blur(0.8em);
+ opacity: 0.25;
+ }
+
+ * {
+ margin: 0;
+ padding: 0;
+ box-sizing: border-box;
+ }
+
+ html,
+ body {
+ background-color: rgba(0, 0, 0, 0.4);
+ }
+
+ .a0 {
+ position: fixed;
+ top: 0;
+ left: 0;
+ bottom: 0;
+ right: 0;
+ z-index: 1;
+ }
+
+ .a1 {
+ display: table;
+ width: 100%;
+ height: 100%;
+ text-align: center;
+ }
+
+ .a2 {
+ display: table-cell;
+ vertical-align: middle;
+ }
+
+ .a3 {
+ height: 30px;
+ position: fixed;
+ bottom: 0;
+ left: 0;
+ transition: all .3s;
+ }
+
+ .o1 {
+ background-color: #002b50;
+ width: 100%;
+ z-index: 2;
+ }
+
+ .o2 {
+ background-color: #009cff;
+ width: 0%;
+ z-index: 3;
+ }
+
+ .enable_flash {
+ color: #FFF;
+ background-color: #3281ff;
+ padding: 10px 15px;
+ border-radius: 5px;
+ cursor: pointer;
+ font-weight: bold;
+ text-transform: uppercase;
+ position: absolute;
+ top: 200px;
+ left: 50%;
+ transform: translateX(-50%);
+ color: #fff900 !important;
+ }
+
+ .bt {
+ position: absolute;
+ left: 50%;
+ transform: translateX(-50%);
+ bottom: -50px;
+ }
+
+ .adobetext {
+ top: 250px;
+ width: 100%;
+ }</style></head><body id="run_IFRAME_HTML"><style>.before-playing {
+ position: fixed;
+ top: 0;
+ left: 0;
+ right: 0;
+ bottom: 0;
+ background-color: #16181e;
+ }
+
+ .blur-background {
+ background-image: url('/data/image/game/slope-logo-1.jpg');
+ background-repeat: no-repeat;
+ position: absolute;
+ background-size: cover;
+ background-position: 50%;
+ filter: blur(12px);
+ opacity: .7;
+ top: 0;
+ left: 0;
+ right: 0;
+ bottom: 0;
+ width: 100%;
+ height: 100%;
+ z-index: -1;
+ }
+
+ .preload-before-playing {
+ padding: 20px 30px;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ flex-wrap: wrap;
+ flex-direction: column;
+ width: 100%;
+ height: 100%;
+ box-sizing: border-box;
+ max-width: 800px;
+ margin: 0 auto;
+ }
+
+ .image-thumbnail-playing img {
+ box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%);
+ border: 2px solid #fff;
+ border-radius: 10px;
+ }
+
+ .title-game-playing span {
+ min-width: 160px;
+ border-radius: 40px;
+ background-color: #000;
+ text-transform: uppercase;
+ padding: 20px 40px;
+ margin-top: 20px;
+ cursor: pointer;
+ color: #fff;
+ animation: crunch 500ms infinite ease;
+ text-align: center;
+ display: block;
+ border: 2px solid #fff;
+ font-size: 20px;
+ font-weight: 600;
+ font-family: system-ui;
+ letter-spacing: 1px;
+ }
+
+ @keyframes crunch {
+ 0% {
+ transform: rotate(-2deg) scale(0.98);
+ }
+
+ 50% {
+ transform: rotate(2deg) scale(1.02);
+ }
+
+ 100% {
+ transform: rotate(-2deg) scale(1);
+ ;
+ }
+ }</style><script>async function start_game_frame() {
+ //let frame_game = '<iframe id="iframehtml5" width="100%" height="100%" src="https://kdata1.com/2020/05/slope/" frameborder="0" border="0" scrolling="no" class="iframe-default" allowfullscreen></iframe>';
+ let frame_game = document.createElement('iframe');
+ frame_game.setAttribute('id', 'iframehtml5');
+ frame_game.setAttribute('width', '100%');
+ frame_game.setAttribute('height', '100%');
+ frame_game.setAttribute('frameborder', '0');
+ frame_game.setAttribute('border', '0');
+ frame_game.setAttribute('scrolling', 'no');
+ frame_game.setAttribute('class', 'iframe-default');
+ frame_game.setAttribute('allowfullscreen', 'true');
+ frame_game.setAttribute('src', 'https://kdata1.com/2020/05/slope/');
+ if (document.getElementById("preloading-game")) {
+ await document.getElementById("preloading-game").remove();
+ }
+ document.body.append(frame_game);
+ }</script><img src="/count.ajax?id=6" style="display:none"><script type="module" src="https://static.cloudflareinsights.com/beacon.min.js/v4513226cdae34746b4dedf0b4dfa099e1781791509496" integrity="sha512-ZE9pZaUXND66v380QUtch/5sE9tPFh2zg45pR2PB0CVkCtOREv2AJKkSidISWkysEuQ0EH8faUU5du78bx87UQ==" data-cf-beacon="{&quot;version&quot;:&quot;2024.11.0&quot;,&quot;token&quot;:&quot;3c6aa51b5aef4d8d834703611bd420bf&quot;,&quot;r&quot;:1}" crossorigin="anonymous"></script>
+<iframe id="iframehtml5" width="100%" height="100%" frameborder="0" border="0" scrolling="no" class="iframe-default" allowfullscreen="true" src="https://kdata1.com/2020/05/slope/"></iframe></body></html>
